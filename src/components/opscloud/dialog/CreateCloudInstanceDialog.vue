67492199285aa13639f27b32166c6da058693dd0
白衣 <baiyi@gegejia.com>
2020-05-28 11:09:04 +0800

    echarts重复数据删除

--- src/components/opscloud/dialog/CreateCloudInstanceDialog.vue
+++ src/components/opscloud/dialog/CreateCloudInstanceDialog.vue
@@ -224,7 +224,6 @@
   let echarts = require('echarts/lib/echarts')
   require('echarts/lib/chart/pie')
   require('echarts/map/js/china')
-  require('echarts/lib/chart/pie')
   require('echarts/lib/chart/line')
   require('echarts/lib/chart/bar')
   require('echarts/lib/component/tooltip')
