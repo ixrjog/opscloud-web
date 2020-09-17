af0a7627d4dcac68271ed4930fd5060f1b6fe13b
xujian <xiuyuan@gegejia.com>
2020-09-17 15:36:38 +0800

    修改默认字体大小和主题

--- src/store/modules/d2admin/modules/size.js
+++ src/store/modules/d2admin/modules/size.js
@@ -5,7 +5,7 @@ export default {
   namespaced: true,
   state: {
     // 尺寸
-    value: '' // medium small mini
+    value: 'mini' // medium small mini
   },
   actions: {
     /**
@@ -66,7 +66,7 @@ export default {
         state.value = await dispatch('d2admin/db/get', {
           dbName: 'sys',
           path: 'size.value',
-          defaultValue: 'default',
+          defaultValue: 'mini',
           user: true
         }, { root: true })
         // 应用
