4a1638d01c0fb18cbf4df72d01abc594c30d25e7
liangjian <liangjian@xinc818.group>
2020-10-28 16:37:21 +0800

    dubbo直连前端页面

--- src/pages/workbench/tcp-mapping/index.vue
+++ src/pages/workbench/tcp-mapping/index.vue
@@ -7,13 +7,15 @@
       <el-col>
         <el-tabs tab-position="top" style="height: 200px;">
           <el-tab-pane label="使用帮助">
-            <d2-markdown :source="doc.previewDoc" v-if="success"/>
-            <d2-highlight :code="doc.previewDoc" v-show="false" lang="sh" style="margin-top: 5px"/>
+            <d2-markdown :source="dubboResolveDoc.previewDoc" v-if="success"/>
+            <d2-highlight :code="dubboResolveDoc.previewDoc" v-show="false" lang="sh" style="margin-top: 5px"/>
           </el-tab-pane>
           <el-tab-pane label="配置详情">
             <el-row :gutter="24" style="margin-bottom: 5px;margin-left: 10px">
               <el-button @click="handlerRefreshDev" class="button" :loading="btnRefreshDevLoding">重新扫描并生成dev配置</el-button>
               <el-button @click="handlerRefreshDaily" :loading="btnRefreshDailyLoding" class="button">重新扫描并生成daily配置</el-button>
+              <el-divider></el-divider>
+              <d2-markdown :source="dubboResolvePreviewDoc.previewDoc" v-if="previewSuccess"/>
             </el-row>
           </el-tab-pane>
         </el-tabs>
@@ -35,23 +37,34 @@
     data () {
       return {
         title: 'Dubbo直连配置',
-        docKey: 'DUBBO_RESOLVE',
-        doc: {},
+        dubboResolveDocKey: 'DUBBO_RESOLVE',
+        dubboResolveDoc: {},
+        dubboResolvePreviewDocKey: 'DUBBO_RESOLVE_PREVIEW',
+        dubboResolvePreviewDoc: {},
         btnRefreshDevLoding: false,
         btnRefreshDailyLoding: false,
-        success: false
+        success: false,
+        previewSuccess: false
       }
     },
     mounted () {
       this.handlerFeatchDoc()
+      this.handlerFeatchPreviewDoc()
     },
     filters: {},
     methods: {
       handlerFeatchDoc () {
-        queryDocByKey(this.docKey)
+        queryDocByKey(this.dubboResolveDocKey)
           .then(res => {
-            this.doc = res.body
+            this.dubboResolveDoc = res.body
             this.success = true
+          })
+      },
+      handlerFeatchPreviewDoc () {
+        queryDocByKey(this.dubboResolvePreviewDocKey)
+          .then(res => {
+            this.dubboResolvePreviewDoc = res.body
+            this.previewSuccess = true
           })
       },
       handlerRefreshDev () {
