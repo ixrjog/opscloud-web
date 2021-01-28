86dfbeea3a4cdf2d6e22ffeb62401f588018e815
修远 <xiuyuan@xinc818.group>
2021-01-28 10:20:20 +0800

    公告跑马灯修改滚动时间

--- src/components/opscloud/announcement/AnnouncementCarousel.vue
+++ src/components/opscloud/announcement/AnnouncementCarousel.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <el-carousel height="150px" v-if="isShow && JSON.stringify(announcementData) !== '[]'" style="margin-bottom: 15px">
+    <el-carousel height="150px" v-if="isShow && JSON.stringify(announcementData) !== '[]'" style="margin-bottom: 15px"
+                 :interval="interval">
       <el-carousel-item v-for="announcement in announcementData" :key="announcement.id">
         <el-row>
           <b>{{ announcement.title }}</b>
@@ -20,7 +21,8 @@ export default {
   data () {
     return {
       announcementData: [],
-      isShow: false
+      isShow: false,
+      interval: 8 * 1000
     }
   },
   name: 'AnnouncementCarousel',
@@ -51,11 +53,11 @@ export default {
   opacity: 0.75;
   line-height: 50px;
   margin: 0;
-  margin-left: 30px;
+  margin-left: 50px;
 }
 
 .el-carousel__item p {
-  margin-left: 30px;
+  margin-left: 50px;
   font-size: 14px;
 }
 
